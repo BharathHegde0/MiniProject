c1fe7abb139e2ad61f18bdd6d4ef00244eef8295
BharathHegde0 <126745913+BharathHegde0@users.noreply.github.com>
2023-07-01 15:50:15 +0530

    Header file, Task with clock

---
+++ Project/header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="header/style.css">
+</head>
+<body>
+    <header>
+        <section class="header-container">
+            
+            <div class="logo">
+                <h1>Task Manager</h1>
+            </div>
+
+            <div class="nav-items">
+                <div>
+                    <a href="header.php"><p>Home</p></a>
+                </div>
+
+                <div>
+                    <a href="taskman.php"><p>Task Manager</p></a>
+                </div>
+                
+                <div>
+                    <a href=""><p>About Us</p></a>
+                </div>
+                
+                <div>
+                    <a href=""><p>Contact Us</p></a>
+                </div>
+
+            </div>
+
+            
+
+        </section>
+    </header>
+
+    
+</body>
+</html>
---
+++ Project/taskman.php
@@ -0,0 +1,16 @@
+<?php include("header.php"); ?>
+
+<link rel="stylesheet" href="tasks/task.css">
+
+<section class="tasks-body">
+    <section class="clock-container">
+        <section class="clock-items">
+            <span class="clock-item" id="clock-hours">00</span>
+            <span class="clock-item" id="clock-minutes">00</span>
+            <span class="clock-item" id="clock-seconds">00</span>
+            <span class="clock-item" id="clock-ampm">PM</span>
+        </section>  
+    </section>
+</section>
+
+<script src="tasks/clock.js"></script>
