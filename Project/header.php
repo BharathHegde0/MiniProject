e4d553341df92f8eb7e837bf326ad7e82de0ed1d
BharathHegde0 <126745913+BharathHegde0@users.noreply.github.com>
2023-07-27 21:42:08 +0530

    Revert "overhaul"
    This reverts commit c5a1a4c9d131493b857859721e3d8faf2e3a0c4c.

--- Project/header.php
+++ Project/header.php
@@ -3,13 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="Cache-control" content="no-cache">
     <title>Document</title>
     <link rel="stylesheet" href="header/style.css">
 </head>
 <body>
     <header>
-        <section class="header-container stick">
+        <section class="header-container">
             
             <div class="logo">
                 <h1>Task Manager</h1>
@@ -17,19 +16,19 @@
 
             <div class="nav-items">
                 <div>
-                    <a href="home.php" class="nav-links"><p>Home</p></a>
+                    <a href="header.php"><p>Home</p></a>
                 </div>
 
                 <div>
-                    <a href="tasks.php" class="nav-links"><p>Task Manager</p></a>
+                    <a href="tasks.php"><p>Task Manager</p></a>
                 </div>
                 
                 <div>
-                    <a href="" class="nav-links"><p>About Us</p></a>
+                    <a href=""><p>About Us</p></a>
                 </div>
                 
                 <div>
-                    <a href="" class="nav-links"><p>Contact Us</p></a>
+                    <a href=""><p>Contact Us</p></a>
                 </div>
 
             </div>
--- Project/tasks.php
+++ Project/tasks.php
@@ -1,14 +1,13 @@
-<?php include("popup.php")?>
 <?php include("header.php")?>
 <?php include("clock.php")?>
 
 <link rel="stylesheet" href="tasks/tasks.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css" integrity="sha384-jLKHWM3JRmfMU0A5x5AkjWkw/EYfGUAGagvnfryNV3F9VqM98XiIH7VBGVoxVSc7" crossorigin="anonymous">
-<center>
-<section class="tasks-page-container" style="padding: 20%; margin: 0px; padding-top: 0%; background-image: linear-gradient(#031d44, #2a75e6); height:47vh;">
+
+<section class="tasks-page-container" style="margin: 20%; margin-top: 2%;">
     <!-- Input Section -->
     <div class="tasks-input">
-        <form action="" id="new-task-form" style="margin-bottom: 0px;">
+        <form action="" id="new-task-form">
             <!-- Enter Text -->
             <input type="text" name="" id="new-task-input" class="tasks-add-area" placeholder="Enter Your Task" autocomplete="off">
 
@@ -16,7 +15,7 @@
             <button class="tasks-add-button" onclick="{return false;}">Add Task</button>
         </form>
     </div>
-    
+
     <section class="tasks-container">
         <!-- To Do -->
         <div class="tasks-board to-do tasks">
@@ -56,8 +55,8 @@
         </div>
         
     </section>
-    </center>
 </section>
+
 <script src="tasks/tasks2.js"></script>
 <!-- <script src="tasks/task_input.js"></script> -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js"></script>
